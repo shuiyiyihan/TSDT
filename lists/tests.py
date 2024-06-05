0de09222257890cb2450bfe4cec6228777ba53ec
shuiyiyihan <314796321@qq.com>
2024-06-05 22:26:30 +0800

    message11:Show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -6,14 +6,35 @@ from django.template.loader import render_to_string
 from lists.models import Item
 # Create your tests here.
 class HomePageTest(TestCase):
-    # def test_root_url_resolves_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func, home_page)
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
     
     def test_uses_home_template(self):
         request = self.client.get('/')
         self.assertTemplateUsed(request, 'home.html')
         #lab8新加
+        
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+        
+    def  test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        
+        
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
         
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
@@ -22,11 +43,6 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
-        
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
         
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from lists.models import Item
+
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    # return render(request, 'home.html'
-    
-    return render(request, 'home.html',{
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method=='POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items=Item.objects.all()
+    return render(request, 'home.html'
+                  ,{'items':items})
 
